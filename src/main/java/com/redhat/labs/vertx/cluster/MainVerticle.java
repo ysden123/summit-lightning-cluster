5f6f22341b83d777952d7405645328a3366ba71f
Yuriy Stul <ysden123@gmail.com>
2018-08-04 22:09:24 +0300

    formatting

--- src/main/java/com/redhat/labs/vertx/cluster/MainVerticle.java
+++ src/main/java/com/redhat/labs/vertx/cluster/MainVerticle.java
@@ -19,18 +19,18 @@ public class MainVerticle extends AbstractVerticle {
     public void start() {
 
         // Schedule the timerFired method to run every 2 seconds + a randomized delay.
-        long delay = Math.round(Math.random()*100 + 2000);
+        long delay = Math.round(Math.random() * 100 + 2000);
         vertx.setPeriodic(delay, this::timerFired);
 
         // Configure a consumer to read events from the event bus and print them to the console.
         vertx.eventBus().consumer("hello.address", this::handleMessage);
     }
 
-    void timerFired(long timerId) {
-        vertx.eventBus().publish("hello.address", "Hello "+UUID);
+    private void timerFired(long timerId) {
+        vertx.eventBus().publish("hello.address", "Hello " + UUID);
     }
 
-    void handleMessage(Message<String> msg) {
+    private void handleMessage(Message<String> msg) {
         LOG.error(msg.body());
     }
 }
